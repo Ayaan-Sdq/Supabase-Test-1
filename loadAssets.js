8350587549f76f900f2d8408c23d0ee3e50037f0
Ayaan S <2014177@apps.nsd.org>
2025-11-28 22:49:25 -0800

    Final Edit. Redoing this with a new branding.

--- loadAssets.js
+++ loadAssets.js
@@ -14,6 +14,8 @@ function updateHTML () {
 
 function updateNavBar () {
 
+    let currentHTML = window.location.pathname.split("/").pop();
+
     if (clientSession) {
 
         document.getElementById("navigationBarDiv").insertAdjacentHTML("beforeend", `
@@ -40,27 +42,48 @@ function updateNavBar () {
 
     } else {
 
+        switch (currentHTML) {
+            case "accountSettings.html":
+            case "chatbotHelp.html":
+            case "dashboard.html":
+            case "helpForm.html":
+            case "requestForm.html": window.location.href = "login.html";
+        }
+
         document.getElementById("navigationBarDiv").insertAdjacentHTML("beforeend", `
             <ul>
 
-                <li><a href="index.html">Home</a></li>
+                <li><a href="index.html" id="Landing Page Link">
+                    <img src="imgLogo.png" width=180>
+                </a></li>
 
-                <li><a href="search.html">Search</a></li>
+                <li><a href="search.html" id="Search Page Link">
+                    <span>Find Your Resources</span>
+                </a></li>
 
-                <li><a href="login.html">Ask Neighbert</a></li>
+                <li><a href="chatbotHelp.html" id="Log In Page Link">
+                    <span>Ask Neighbert</span>
+                </a></li>
 
-                <li class="dropdown">
-                    <a href="#">Account</a>
-                    <ul class="dropdownMenu">
-                        <li><a href="signup.html">Sign Up</a></li>
-                        <li><a href="login.html">Log In</a></li>
-                        <li><a href="signup.html">Request Update</a></li>
-                    </ul>
-                </li>
+                <li><a href="requestForm.html" id="Request Update Page Link">
+                    <span>Request Update</span>
+                </a></li>
+
+                <li><a href="login.html" id="Log In Page Link">
+                        <span>Log In</span>
+                </a></li>
 
             </ul>    
         `);
         
     }
+
+    document.querySelectorAll("nav a").forEach(a => {
+        let link = a.getAttribute("href");
+
+        if (link === currentHTML) {
+            a.closest("li").classList.add("active");
+        }
+    });
 
 }
